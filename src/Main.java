c6befc0b1b85f6a0cd038a905de326e83df68e11
krotskas <kostasrotskas@gmail.com>
2022-11-30 12:01:45 +0200

    Apo IntelliJ IDEA

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        FAME cpabe = new FAME();
-        Instant start_init = Instant.now();
-        FAMEMasterKey msk = cpabe.setup();
-        Instant end_init = Instant.now();
-        System.out.println("~~~~ Setup Complete ~~~~");
-        System.out.println("elapsed time:" + Duration.between(start_init, end_init) + "\n");
+
     }
 
 
